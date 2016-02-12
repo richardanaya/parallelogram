58a213124ad5165f1443bbcfaf9d9a0b222a98fc
Richard <richard.anaya@gmail.com>
2016-02-11 18:39:31 -0600

    first commit

---
+++ parallelogram.js
@@ -0,0 +1,13 @@
+// parallelogram.js
+// repo    : https://github.com/richardanaya/parallelogram
+// license : MIT
+
+(function (window, module) {
+  "use strict";
+  window.parallelogram = module.exports = function(){
+    
+  }
+})(
+  typeof window !== "undefined" ? window : {},
+  typeof module !== "undefined" ? module : {}
+);
