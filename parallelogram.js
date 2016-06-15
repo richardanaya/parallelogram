e1ec8698d99c887313a7ea8be35571432a0a3d48
Richard <richard.anaya@gmail.com>
2016-06-14 19:55:13 -0500

    oops

--- parallelogram.js
+++ parallelogram.js
@@ -37,7 +37,7 @@
 
     var depsAcquired = function(deps){
       //put together the web worker
-      var response = deps.join(";\n")+
+      var response = deps.join(";\n")+";\n"+
       "var RETURN = 0;\n"+
       "var CALLBACK = 1;\n"+
       "function isObject(obj){return obj === Object(obj);}\n"+
